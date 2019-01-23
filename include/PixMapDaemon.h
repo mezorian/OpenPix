25e958bc52b635dc65a31a1c7873b0f50fd4b54e
mezorian <mezorian@inc-ffh.solutions>
2019-01-23 20:27:02 +0100

    added PixMapDaemon

---
+++ include/PixMapDaemon.h
@@ -0,0 +1,19 @@
+/**
+ * PixMapDaemon.h
+ * Purpose: defines class PixMapDaemon
+ *
+ * @author mezorian
+ * @version 0.1
+ */
+
+#ifndef PIXMAPDAEMON_H
+#define PIXMAPDAEMON_H
+
+
+class PixMapDaemon
+{
+public:
+    PixMapDaemon();
+};
+
+#endif // PIXMAPDAEMON_H
---
+++ src/PixMapDaemon.cpp
@@ -0,0 +1,14 @@
+/**
+ * PixMapDaemon.cpp
+ * Purpose: implements class PixMapDaemon
+ *
+ * @author Wall.Of.Death
+ * @version 2.2
+ */
+
+#include "PixMapDaemon.h"
+
+PixMapDaemon::PixMapDaemon()
+{
+
+}
