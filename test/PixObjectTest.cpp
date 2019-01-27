558b5e8a24c35ce0fd2557df6f5d3968b4d80803
mezorian <mezorian@inc-ffh.solutions>
2019-01-27 17:20:45 +0100

    added testcases for PixObject-class

--- test/PixObjectTest.cpp
+++ test/PixObjectTest.cpp
@@ -8,8 +8,240 @@
 
 #include "catch.hpp"
 #include "PixObject.h"
+#include "DotPixObject.h"
 
-TEST_CASE("constructor testing") {
+TEST_CASE("getter / setter testing") {
+    PixObject pixObject;
+    int x,y,pixMapDaemonWidth,pixMapDaemonHeight;
 
+    SECTION("big negative values") {
+        x=-16678;
+        y=-323424;
+        pixMapDaemonWidth=-13453454;
+        pixMapDaemonHeight=-323423234;
+    }
+
+    SECTION("small negative values") {
+        x=-1;
+        y=-34;
+        pixMapDaemonWidth=-10;
+        pixMapDaemonHeight=-50;
+    }
+
+    SECTION("zeros") {
+        x=0;
+        y=0;
+        pixMapDaemonWidth=0;
+        pixMapDaemonHeight=0;
+    }
+
+    SECTION("small values") {
+        x=1;
+        y=34;
+        pixMapDaemonWidth=10;
+        pixMapDaemonHeight=50;
+    }
+
+    SECTION("big values") {
+        x=1678;
+        y=3244;
+        pixMapDaemonWidth=13453434;
+        pixMapDaemonHeight=3234244234;
+    }
+
+    SECTION("values out of bounds is also allowed") {
+        x=10;
+        y=10;
+        pixMapDaemonWidth=5;
+        pixMapDaemonHeight=5;
+    }
+
+    pixObject.setPixMapDaemonWidth(pixMapDaemonWidth);
+    pixObject.setPixMapDaemonHeight(pixMapDaemonHeight);
+    pixObject.setX(x);
+    pixObject.setY(y);
+    REQUIRE(pixObject.getX() == x);
+    REQUIRE(pixObject.getY() == y);
+    REQUIRE(pixObject.getPixMapDaemonWidth() == pixMapDaemonWidth);
+    REQUIRE(pixObject.getPixMapDaemonHeight() == pixMapDaemonHeight);
 }
 
+TEST_CASE("move testing") {
+    PixObject pixObject;
+    pixObject.setPixObjectType(new DotPixObject());
+    int x,y,pixMapDaemonWidth,pixMapDaemonHeight,deltaX,deltaY;
+
+    SECTION ("don't move") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 0;
+        deltaY = 0;
+    }
+
+    SECTION ("move one step up") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 0;
+        deltaY = -1;
+    }
+
+    SECTION ("move one step down") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 0;
+        deltaY = +1;
+    }
+
+    SECTION ("move one step left") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = -1;
+        deltaY = 0;
+    }
+
+    SECTION ("move one step right") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 1;
+        deltaY = 0;
+    }
+
+    SECTION ("move multiple steps up") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 0;
+        deltaY = -5;
+    }
+
+    SECTION ("move multiple steps down") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 0;
+        deltaY = +3;
+    }
+
+    SECTION ("move multiple steps left") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = -2;
+        deltaY = 0;
+    }
+
+    SECTION ("move multiple steps right") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 4;
+        deltaY = 0;
+    }
+
+    SECTION ("move diagonal right up") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 1;
+        deltaY = -1;
+    }
+
+    SECTION ("move diagonal right down") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 1;
+        deltaY = 1;
+    }
+
+    SECTION ("move diagonal left up") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = -1;
+        deltaY = -1;
+    }
+
+    SECTION ("move diagonal left down") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = -1;
+        deltaY = 1;
+    }
+
+    SECTION ("move out of pixmap right") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 100;
+        deltaY = -1;
+    }
+
+    SECTION ("move out of pixmap left") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = -100;
+        deltaY = -1;
+    }
+
+    SECTION ("move out of pixmap up") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 1;
+        deltaY = -100;
+    }
+
+    SECTION ("move out of pixmap down") {
+        x = 5;
+        y = 5;
+        pixMapDaemonWidth = 10;
+        pixMapDaemonHeight = 10;
+        deltaX = 1;
+        deltaY = 100;
+    }
+
+    pixObject.setX(x);
+    pixObject.setY(y);
+    pixObject.create();
+    pixObject.setPixMapDaemonWidth(pixMapDaemonWidth);
+    pixObject.setPixMapDaemonHeight(pixMapDaemonHeight);
+    int oldX = pixObject.getX();
+    int oldY = pixObject.getY();
+
+    pixObject.move(deltaY,deltaX);
+    REQUIRE(pixObject.getX() >= 0);
+    REQUIRE(pixObject.getX() < pixMapDaemonWidth);
+    REQUIRE(pixObject.getY() >= 0);
+    REQUIRE(pixObject.getY() < pixMapDaemonHeight);
+    if ( (oldX + deltaX >=0) && (oldX + deltaX < pixMapDaemonWidth) ) {
+         REQUIRE(pixObject.getX() == oldX + deltaX);
+    }
+    if ( (oldY + deltaY >=0) && (oldY + deltaY < pixMapDaemonHeight) ) {
+         REQUIRE(pixObject.getY() == oldY + deltaY);
+    }
+
+}
--- test/PixTest.cpp
+++ test/PixTest.cpp
@@ -56,56 +56,56 @@ TEST_CASE("Pix constructor / getter / setter / operator testing") {
     }
 
     /* standard constructor / getter / setter testing */
-    Pix pixObject(active,red,green,blue);
-    REQUIRE(pixObject.getActive() == active);
-    REQUIRE(pixObject.getRed() >= 0);
-    REQUIRE(pixObject.getGreen() >= 0);
-    REQUIRE(pixObject.getBlue() >= 0);
-    REQUIRE(pixObject.getRed() <= 255);
-    REQUIRE(pixObject.getGreen() <= 255);
-    REQUIRE(pixObject.getBlue() <= 255);
+    Pix pix(active,red,green,blue);
+    REQUIRE(pix.getActive() == active);
+    REQUIRE(pix.getRed() >= 0);
+    REQUIRE(pix.getGreen() >= 0);
+    REQUIRE(pix.getBlue() >= 0);
+    REQUIRE(pix.getRed() <= 255);
+    REQUIRE(pix.getGreen() <= 255);
+    REQUIRE(pix.getBlue() <= 255);
 
     if (red < 0) {
-        REQUIRE(pixObject.getRed() == 0);
+        REQUIRE(pix.getRed() == 0);
     } else if (red > 255) {
-        REQUIRE(pixObject.getRed() == 255);
+        REQUIRE(pix.getRed() == 255);
     } else {
-        REQUIRE(pixObject.getRed() == red);
+        REQUIRE(pix.getRed() == red);
     }
 
     if (green < 0) {
-        REQUIRE(pixObject.getGreen() == 0);
+        REQUIRE(pix.getGreen() == 0);
     } else if (green > 255) {
-        REQUIRE(pixObject.getGreen() == 255);
+        REQUIRE(pix.getGreen() == 255);
     } else {
-        REQUIRE(pixObject.getGreen() == green);
+        REQUIRE(pix.getGreen() == green);
     }
 
     if (blue < 0) {
-        REQUIRE(pixObject.getBlue() == 0);
+        REQUIRE(pix.getBlue() == 0);
     } else if (blue > 255) {
-        REQUIRE(pixObject.getBlue() == 255);
+        REQUIRE(pix.getBlue() == 255);
     } else {
-        REQUIRE(pixObject.getBlue() == blue);
+        REQUIRE(pix.getBlue() == blue);
     }
 
     /* --- copy constructor testing --- */
-    Pix pixObject2(pixObject);
+    Pix pix2(pix);
 
-    REQUIRE_FALSE(&pixObject2 == &pixObject);
-    REQUIRE(pixObject2.getActive() == pixObject.getActive());
-    REQUIRE(pixObject2.getRed() == pixObject.getRed());
-    REQUIRE(pixObject2.getGreen() == pixObject.getGreen());
-    REQUIRE(pixObject2.getBlue() == pixObject.getBlue());
+    REQUIRE_FALSE(&pix2 == &pix);
+    REQUIRE(pix2.getActive() == pix.getActive());
+    REQUIRE(pix2.getRed() == pix.getRed());
+    REQUIRE(pix2.getGreen() == pix.getGreen());
+    REQUIRE(pix2.getBlue() == pix.getBlue());
 
     /* assignment operator testing --- */
-    Pix pixObject3(pixObject);
+    Pix pix3(pix);
 
-    REQUIRE_FALSE(&pixObject3 == &pixObject);
-    REQUIRE(pixObject3.getActive() == pixObject.getActive());
-    REQUIRE(pixObject3.getRed() == pixObject.getRed());
-    REQUIRE(pixObject3.getGreen() == pixObject.getGreen());
-    REQUIRE(pixObject3.getBlue() == pixObject.getBlue());
+    REQUIRE_FALSE(&pix3 == &pix);
+    REQUIRE(pix3.getActive() == pix.getActive());
+    REQUIRE(pix3.getRed() == pix.getRed());
+    REQUIRE(pix3.getGreen() == pix.getGreen());
+    REQUIRE(pix3.getBlue() == pix.getBlue());
 
 }
 
