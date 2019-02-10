b84f7ce4d46ab2a0e006e4f73670dd7ae709cea0
mezorian <mezorian@inc-ffh.solutions>
2019-02-10 22:44:56 +0100

    this closes #19 : renamed pixmap to pixMap

--- src/OpenPixGameEngine.cpp
+++ src/OpenPixGameEngine.cpp
@@ -68,7 +68,7 @@ void OpenPixGameEngine::run() {
             elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
         }
 
-        // when the timer interval is finished read the inputs, execute the game logic and paint the pixmap to the output device
+        // when the timer interval is finished read the inputs, execute the game logic and paint the pixMap to the output device
         cout << "beep " << i  << "x" << elapsed.count() << endl;
         readInputs();
         executeGameLogic();
@@ -83,7 +83,7 @@ void OpenPixGameEngine::run() {
 
 /**
  * OpenPixGameEngine::repaint
- * @brief paints the current pixmap to the output device
+ * @brief paints the current pixMap to the output device
  *
  * This function calls the repaint-method of the pixMapOutputDriver.
  * If the QT-UI is used as output driver this function also triggers to process all events of the QT-UI
@@ -134,7 +134,7 @@ void OpenPixGameEngine::setExecuteGameLogicInterval(unsigned int value_) {
 /**
  * OpenPixGameEngine::getRepaintInterval
  * @brief getter of member-variable repaintInterval
- * @return returns the interval of the repaint-timer, which means the amount of milli-seconds between the pixmap is painted to the output device
+ * @return returns the interval of the repaint-timer, which means the amount of milli-seconds between the pixMap is painted to the output device
  */
 unsigned int OpenPixGameEngine::getRepaintInterval() const {
     return repaintInterval;
@@ -143,7 +143,7 @@ unsigned int OpenPixGameEngine::getRepaintInterval() const {
 /**
  * OpenPixGameEngine::setRepaintInterval
  * @brief setter of member-variable repaintInterval
- * @param value_ the interval of the repaint-timer, which means the amount of milli-seconds between the pixmap is painted to the output device
+ * @param value_ the interval of the repaint-timer, which means the amount of milli-seconds between the pixMap is painted to the output device
  */
 void OpenPixGameEngine::setRepaintInterval(unsigned int value_) {
     repaintInterval = value_;
